8888ae3bc3636b7de127703b73aa54f0da9401df
Ashton996 <aramseur9620@student.clevelandcountyschools.org>
2025-01-21 11:32:57 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const enlargeTableButton = document.getElementById('enlargeTableButton');
+const table = document.querySelector('table');
+enlargeTableButton.addEventListener('click', function() {
+    table.style.width = '60%';
